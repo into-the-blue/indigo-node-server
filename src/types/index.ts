c9962fd9843eaf854b706a2597b437ba81f523fd
Origami <q@timvel.com>
2020-01-31 09:42:31 +0800

    type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,2 @@
-/*
- * File: /Users/origami/Desktop/templates/node-express-template/src/models/index.ts
- * Project: /Users/origami/Desktop/templates/node-express-template
- * Created Date: Friday July 12th 2019
- * Author: Rick yang tongxue(🍔🍔) (origami@timvel.com)
- * -----
- * Last Modified: Friday July 12th 2019 3:23:48 pm
- * Modified By: Rick yang tongxue(🍔🍔) (origami@timvel.com)
- * -----
- */
-export * from './models';
+export * from './common'
+export * from './models'
