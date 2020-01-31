a09364a8a204b6edaeaae73e04059efafdfe8f85
Origami <q@timvel.com>
2020-01-31 18:16:33 +0800

    type

--- src/types/common.ts
+++ src/types/common.ts
@@ -184,6 +184,10 @@ interface IMetroStationDEPRECATED {
   lineId: string
   url: string
 }
+
+export interface IMetroStationClient extends IMetroStation {
+  lines: IMetroLine[]
+}
 export interface IMetroStation extends IMetroStationDEPRECATED {
   id: string
   stationName: string
