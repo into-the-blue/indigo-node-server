b683ca508ab129fb71defd0f3f9c321c4fe2f507
Origami <q@timvel.com>
2020-06-11 14:45:45 +0800

    rate limiter

--- src/apiv1/subscription/utils/errors.ts
+++ src/apiv1/subscription/utils/errors.ts
@@ -1,5 +1,11 @@
 export class SubscriptionInvalidValue extends Error {
   constructor() {
-    super('Invalid Value')
+    super('Invalid Value');
+  }
+}
+
+export class SubscriptionExceedQuota extends Error {
+  constructor() {
+    super('Exceed quota');
   }
 }
--- src/app.ts
+++ src/app.ts
@@ -8,100 +8,61 @@
  * Modified By: Rick yang tongxue(🍔🍔) (origami@timvel.com)
  * -----
  */
-require('module-alias/register')
-import 'reflect-metadata'
-import Koa from 'koa'
-import bodyParser from 'koa-bodyparser'
-import session from 'koa-session'
-import passport from 'koa-passport'
-import cors from '@koa/cors'
-import { useKoaServer } from 'routing-controllers'
-import ApiV1Controller from './apiv1'
-import { Mongo } from './db'
-import graphqlMiddleware from './graphql'
-import { createRateLimiter, setupPassport } from './middleware'
-import { setupDashBoard } from './dashboard'
-import { logger, randomString, isMaster, isDevEnv } from './utils'
-import StartCronJob from './cronJobs'
-import Router from 'koa-router'
-import helmet from 'koa-helmet'
-import {} from 'rate-limiter-flexible'
+require('module-alias/register');
+import 'reflect-metadata';
+import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
+import session from 'koa-session';
+import passport from 'koa-passport';
+import cors from '@koa/cors';
+import { useKoaServer } from 'routing-controllers';
+import ApiV1Controller from './apiv1';
+import { Mongo } from './db';
+import graphqlMiddleware from './graphql';
+import { createRateLimiter, setupPassport } from './middleware';
+import { setupDashBoard } from './dashboard';
+import { logger, randomString, isMaster, isDevEnv } from './utils';
+import StartCronJob from './cronJobs';
+import Router from 'koa-router';
+import helmet from 'koa-helmet';
+import {} from 'rate-limiter-flexible';
 
-const NOT_FOUND_MSG = 'What are you looking for ?'
-const router = new Router<Koa.DefaultState, Koa.Context>()
-const rateLimiter = createRateLimiter()
-const PORT = process.env.PORT || 7000
+const NOT_FOUND_MSG = 'What are you looking for ?';
+const router = new Router<Koa.DefaultState, Koa.Context>();
+const rateLimiter = createRateLimiter();
+const PORT = process.env.PORT || 7000;
 
 const run = async () => {
-  await Mongo.connect()
+  await Mongo.connect();
 
   // cron job
-  StartCronJob()
+  StartCronJob();
 
-  const app = new Koa<Koa.DefaultState, Koa.Context>()
-  app.use(helmet())
-  app.use(cors())
-  app.use(bodyParser())
-  require('koa-qs')(app)
+  const app = new Koa<Koa.DefaultState, Koa.Context>();
+  app.use(helmet());
+  app.use(cors());
+  app.use(bodyParser());
+  require('koa-qs')(app);
   // session
-  app.keys = [process.env.SESSION_SECRET]
-  app.use(session({}, app))
+  app.keys = [process.env.SESSION_SECRET];
+  app.use(session({}, app));
 
   // passport
-  setupPassport(app)
+  setupPassport(app);
 
-  // limiter
+  // log
   app.use(async (ctx, next) => {
-    const requestId = randomString(10)
+    const requestId = randomString(10);
     try {
-      console.time(requestId + ctx.url)
-      if (!isDevEnv) await rateLimiter.consume(ctx.ip)
-      await next()
+      console.time(requestId + ctx.url);
+      await next();
     } catch (err) {
-      if (err.remainingPoints === 0) {
-        ctx.status = 429
-        ctx.body = 'Too Many Requests'
-        return
-      }
-      console.warn(err.message)
-      throw err
+      console.warn(err.message);
+      throw err;
     } finally {
-      console.timeEnd(requestId + ctx.url)
+      console.timeEnd(requestId + ctx.url);
     }
-  })
-
-  //dashboard
-  setupDashBoard(app)
-
-  // graphql
-  app.use(graphqlMiddleware())
-
-  // app.use((ctx, next) => {
-  //   return passport.authenticate(
-  //     'jwt',
-  //     { session: false },
-  //     async (err, user, info, status) => {
-  //       try {
-  //         // console.log({
-  //         //   err,
-  //         //   user,
-  //         //   info,
-  //         //   status,
-  //         // })
-  //         await ctx.login(user)
-  //         ctx.user = user
-  //         await next()
-  //       } catch (err) {
-  //         console.log('errr', err.message)
-  //         ctx.status = 401
-  //         ctx.message = 'Unauthorized'
-  //         return ctx
-  //       }
-  //     }
-  //   )(ctx, next)
-  // })
-
-  app.use(router.routes())
+  });
 
   // routing controller
   useKoaServer(app, {
@@ -114,26 +75,74 @@ const run = async () => {
           { session: false },
           async (err, user, info, status) => {
             try {
-              if (err || !user) return resolve(false)
-              await action.context.login(user)
-              action.context.user = user
-              resolve(true)
+              if (err || !user) return resolve(false);
+              await action.context.login(user);
+              action.context.user = user;
+              resolve(true);
             } catch (err) {
-              console.warn(err.message)
-              resolve(false)
+              console.warn(err.message);
+              resolve(false);
             }
           }
-        )(action.context, action.next as any)
-      })
+        )(action.context, action.next as any);
+      });
     },
     // currentUserChecker: action => action.context.user,
-  })
+  });
+
+  // limiter
+  app.use(async (ctx, next) => {
+    try {
+      if (!ctx.isAuthenticated() && !isDevEnv) {
+        await rateLimiter.consume(ctx.ip);
+      }
+
+      await next();
+    } catch (err) {
+      if (err.remainingPoints === 0) {
+        ctx.status = 429;
+        ctx.body = {
+          msg: 'Too Many Requests',
+          success: false,
+        };
+        return;
+      }
+      console.warn(err.message);
+      throw err;
+    }
+  });
+  //dashboard
+  setupDashBoard(app);
+
+  // graphql
+  app.use(graphqlMiddleware());
+
+  // app.use((ctx, next) => {
+  //   return passport.authenticate(
+  //     'jwt',
+  //     { session: false },
+  //     async (err, user, info, status) => {
+  //       try {
+  //         await ctx.login(user);
+  //         ctx.user = user;
+  //         await next();
+  //       } catch (err) {
+  //         console.log('errr', err.message);
+  //         ctx.status = 401;
+  //         ctx.message = 'Unauthorized';
+  //         return ctx;
+  //       }
+  //     }
+  //   )(ctx, next);
+  // });
+
+  app.use(router.routes());
 
   app.listen(PORT, async () => {
-    logger.info(`server is running at http://localhost:${PORT}`)
-  })
-}
+    logger.info(`server is running at http://localhost:${PORT}`);
+  });
+};
 
 run().catch((err) => {
-  logger.error('failed to start server ' + err.message)
-})
+  logger.error('failed to start server ' + err.message);
+});
